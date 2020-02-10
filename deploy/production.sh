f197be4026d653ef52ade78355b2d58ad4eca01a
quan do <quan.do@asiantech.vn>
2020-02-10 16:04:11 +0700

    node

--- deploy/production.sh
+++ deploy/production.sh
@@ -6,9 +6,10 @@
 # echo "success!"
 
 #!/bin/bash
+npm install
 ssh -o StrictHostKeyChecking=no deploy@178.128.22.245 "cd /var/www/timble-tiny-web && rm -rf ./api/*"
 scp -r -o StrictHostKeyChecking=no ./* deploy@178.128.22.245:/var/www/timble-tiny-web/api
-ssh -o StrictHostKeyChecking=no deploy@178.128.22.245 "cd /var/www/timble-tiny-web && DEBUG='*' pm2 start src/index.js --name api"
+ssh -o StrictHostKeyChecking=no deploy@178.128.22.245 "cd /var/www/timble-tiny-web/api && DEBUG='*' pm2 start src/index.js --name api"
 
 # set -e
 #   reset="\033[0m"
